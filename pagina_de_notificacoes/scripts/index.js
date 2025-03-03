1e91fb799b5fcfdccae91cbcacfe597aa670c376
cat0blu <Cat.Blu.Roger00@gmail.com>
2025-03-03 15:21:45 -0300

    3° test mobile

--- pagina_de_notificacoes/scripts/index.js
+++ pagina_de_notificacoes/scripts/index.js
@@ -3,6 +3,8 @@ let num_notification = document.querySelector('#num_notification')
 let text_all_read = document.querySelector("#text_all_read")
 let msgText = document.querySelector("#msgText")
 
+window.alert('3°test mobile')
+
 elements.forEach((element)=>{
     element.addEventListener('click', ()=>{
         let pontRed = element.querySelector('.campo_icon')
