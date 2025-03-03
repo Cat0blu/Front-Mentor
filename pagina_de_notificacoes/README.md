354f1e2a74baac1fb676b12fbd727b18e60ebba8
cat0blu <Cat.Blu.Roger00@gmail.com>
2025-03-03 14:54:44 -0300

    project: "pagina _de_notificacoes" 1°teste mobile

---
+++ pagina_de_notificacoes/README.md
@@ -0,0 +1,34 @@
+# PROJECT: Página de Notificações #
+
+## Sobre: - Esse projeto simula uma página de notificações. projeto utilizando recursos mais basicos do desenvolvimento web.
+
+
+- Ferramentas utilizadas
+    - Html(semantica)
+    - Css(estilos)
+    - Javscript(funcionalidades, interatividade)
+  
+  
+## Paletas de cores:
+
+- Primary
+
+    - Red: hsl(1, 90%, 64%)
+    - Blue: hsl(219, 85%, 26%)
+
+
+- Neutral
+
+    - White: hsl(0, 0%, 100%)
+    - Very light grayish blue: hsl(210, 60%, 98%)
+    - Light grayish blue 1: hsl(211, 68%, 94%)
+    - Light grayish blue 2: hsl(205, 33%, 90%)
+    - Grayish blue: hsl(219, 14%, 63%)
+    - Dark grayish blue: hsl(219, 12%, 42%)
+    - Very dark blue: hsl(224, 21%, 14%)    
+
+
+## Tipografias, Fontes    
+
+   - Family: [Plus Jakarta Sans](https://fonts.google.com/specimen/Plus+Jakarta+Sans)
+   - Weights: 500, 800
---
+++ pagina_de_notificacoes/scripts/index.js
@@ -0,0 +1,41 @@
+let elements = document.querySelectorAll('main > *')
+let num_notification = document.querySelector('#num_notification')
+let text_all_read = document.querySelector("#text_all_read")
+let msgText = document.querySelector("#msgText")
+
+elements.forEach((element)=>{
+    element.addEventListener('click', ()=>{
+        let pontRed = element.querySelector('.campo_icon')
+        if(pontRed && window.getComputedStyle(pontRed).display !== "none")
+        {
+            pontRed.style.display = 'none'
+            num_notification.innerHTML = Number(num_notification.innerHTML) -1
+            element.style.backgroundColor = 'white'
+        }
+
+        if(element.nextElementSibling && window.getComputedStyle(element.nextElementSibling).display === 'none')
+            {
+                msgText.style.display = 'block'
+            }
+
+        else 
+        {
+            msgText.style.display = 'none'
+        }
+       
+    })
+})
+
+text_all_read.addEventListener('click', ()=>
+{
+    elements.forEach((element)=>{
+        let campo_icon = element.querySelector('.campo_icon')
+        if(campo_icon)
+        {
+            campo_icon.style.display = 'none'
+            element.style.backgroundColor = 'white'
+        }
+        
+    })
+    num_notification.innerHTML = 0
+})
