d5e7e0b69e5f93790bec4d484317ce83f77fa8ac
cat0blu <Cat.Blu.Roger00@gmail.com>
2025-03-18 22:08:28 -0300

    Project "Componente _resumo_resultados"- end

--- Componente_resumo_resultados/scripts/index.js
+++ Componente_resumo_resultados/scripts/index.js
@@ -4,45 +4,50 @@ let array = [{nome: "Reaction", num: 80}, {nome:"Memory",num: 92}, {nome:"Verbal
 let texts_results = [...document.querySelectorAll(".text_result")]
 let nums_results =[...document.querySelectorAll(".num_result")]
 
-let main_conteudo = document.getElementById("main_conteudo")
-console.log(main_conteudo.children)
 
-console.log(texts_results)
-console.log(array)
-console.log(array[0].nome)
+const arquivo_json = "https://cat0blu.github.io/Front-Mentor/Componente_resumo_resultados/scripts/data.json"
 
-texts_results.map((el,i)=>{
-    el.innerHTML = array[i].nome 
-})
+async function carregarJson()
+{
+    const resp = await fetch(arquivo_json)
 
-nums_results.map((el,i)=>{
-    el.innerHTML = `<strong>${array[i].num}</strong>` + " / 100"
-})
+try {
+    if(resp.status === 200)
+        {   
+            const obj =  await resp.json()
+            document.querySelector("#reaction").querySelector(".text_result").innerHTML = obj[0].title
 
-//
+            console.log("rodando aplicação com arquivo .json")
 
-// const arquivo_json = "https://cat0blu.github.io/Front-Mentor/Painel_controle_de_tempo/public/scripts/data.json"
+            texts_results.map((el,i)=>{
+                   el.innerHTML = obj[i].category
+                 })
+    
+            nums_results.map((el,i)=>{
+                el.innerHTML = `<strong>${obj[i].score}</strong>` + " / 100"
+            })     
+        }
+    
+        else 
+        {
+            console.log("[Error] deve ter havido um  [Erro-404] ao fazer a requisição")
+        }
+   
+}
 
-// async function carregarJson()
-// {
-//     const resp = await fetch(arquivo_json)
+catch 
+{
+   console.log("Não foi possivel rodar a aplicação com o arquivo .json")
+    texts_results.map((el,i)=>{
+        el.innerHTML = array[i].nome 
+    })
 
-//     if(resp.status === 200)
-//     {   
-//         // console.log(resp.json())
-//         const obj =  await resp.json()
-//         document.querySelector("#reaction").querySelector(".text_result").innerHTML = obj[0].title
-//         let textess = [...document.querySelectorAll(".text_result")]
-//         textess.map((el,i)=>{
-//                el.innerHTML = obj[i].title 
-//              })
-//     }
+    nums_results.map((el,i)=>{
+        el.innerHTML = `<strong>${array[i].num}</strong>` + " / 100"
+    })
+}
+    
+}
 
-//     else 
-//     {
-//         console.log("[Error]")
-//     }
+carregarJson() 
 
-// }
-
-// carregarJson()
